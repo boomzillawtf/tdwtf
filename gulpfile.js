fc93fd0f3befccb83d1c663d8ad6c653bd8e9f91
error <error@error-unlimited.net>
2016-05-27 13:15:35 -0500

    add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 const gulp = require( 'gulp' ),
+	jshint = require( 'gulp-jshint' ),
 	karma = require( 'karma' );
 
-gulp.task( 'default', [ 'test' ] );
+gulp.task( 'default', [ 'lint', 'test' ] );
+
+gulp.task( 'lint:jshint', () =>
+	gulp.src( [ '*.html', '*.js', 'test/**/*.js', '!gulpfile.js' ] )
+	.pipe( jshint.extract( 'auto' ) )
+	.pipe( jshint() )
+	.pipe( jshint.reporter( 'default', { verbose: true } ) )
+	.pipe( jshint.reporter( 'fail' ) )
+);
+
+gulp.task( 'lint', [ 'lint:jshint' ] );
 
 gulp.task( 'test:karma', done => {
 	const configFile = `${__dirname}/test/karma.conf.js`;
