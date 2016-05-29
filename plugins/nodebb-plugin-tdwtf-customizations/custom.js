1b55943012953b32ab760e6644decec46bc65caf
Ben Lubar <ben.lubar@gmail.com>
2016-05-28 23:44:01 -0500

    convert custom.css and header.html to a plugin

--- plugins/nodebb-plugin-tdwtf-customizations/custom.js
+++ plugins/nodebb-plugin-tdwtf-customizations/custom.js
@@ -1,9 +1,4 @@
-<meta name="google-site-verification" content="CHVbCxly52Dog4tN9fsbqoQkNTASojg2LzYSeJzqRgw" />
-
-<!-- change background colours 
-https://community.nodebb.org/topic/8322/custom-background-using-custom-class-custom-css/2
--->
-<script>
+// https://community.nodebb.org/topic/8322/custom-background-using-custom-class-custom-css/2
 $(window).on('action:ajaxify.contentLoaded', function() {
     if (ajaxify.data && ajaxify.data.cid) {
         $('html').attr('data-category-id', ajaxify.data.cid);
@@ -27,6 +22,3 @@ function details_shim(a){if(!(a&&"nodeType"in a&&"tagName"in a))return details_s
 c=-1!=c.indexOf("open")&&-1==c.indexOf('open=""')?"open":"closed";a.setAttribute("data-open",c);a.className+=" details_shim_"+c;b.addEventListener?b.addEventListener("click",function(){details_shim.toggle(a)}):b.attachEvent&&b.attachEvent("onclick",function(){details_shim.toggle(a)});Object.defineProperty(a,"open",{get:function(){return"open"==this.getAttribute("data-open")},set:function(a){details_shim.toggle(this,a)}});for(b=0;b<a.childNodes.length;b++)if(3==a.childNodes[b].nodeType&&/[^\s]/.test(a.childNodes[b].data)){var c=
 document.createElement("span"),d=a.childNodes[b];a.insertBefore(c,d);a.removeChild(d);c.appendChild(d)}}details_shim.toggle=function(a,b){b="undefined"===typeof b?"open"==a.getAttribute("data-open")?"closed":"open":b?"open":"closed";a.setAttribute("data-open",b);a.className=a.className.replace(/\bdetails_shim_open\b|\bdetails_shim_closed\b/g," ")+" details_shim_"+b};details_shim.init=function(){for(var a=document.getElementsByTagName("summary"),b=0;b<a.length;b++)details_shim(a[b])};
 window.addEventListener?window.addEventListener("load",details_shim.init,!1):window.attachEvent&&window.attachEvent("onload",details_shim.init);
-
-</script>
-
---
+++ plugins/nodebb-plugin-tdwtf-customizations/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+	"meta": function(tags, callback) {
+		callback(null, tags.concat([{
+			name: 'google-site-verification',
+			content: 'CHVbCxly52Dog4tN9fsbqoQkNTASojg2LzYSeJzqRgw'
+		}]));
+	}
+};
