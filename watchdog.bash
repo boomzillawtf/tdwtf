f6540eb0a5fed5830f59762378d835ca474a4a4c
Ben Lubar <ben.lubar@gmail.com>
2016-10-30 10:33:11 -0500

    watchdog.bash: handle parent process exiting on its own, handle parent process hanging before startup completes (>15 minute grace period)

--- watchdog.bash
+++ watchdog.bash
@@ -2,8 +2,16 @@
 
 start_time=$(date -u +%s)
 
+failures=0
+
 until curl -fsm 5 http://127.0.0.1:"$1"/recent.rss > /dev/null; do
-	echo "Waiting for $PPID/$1"
+	kill -0 $PPID &> /dev/null || (echo "$PPID/$1 died unexpectedly"; exit)
+	failures=$(( $failures + 1 ))
+	echo "Waiting for $PPID/$1 ($failures/200)"
+	if [[ $failures -eq 200 ]]; then
+		kill -9 "$PPID"
+		exit
+	fi
 	sleep 5
 done
 
@@ -14,6 +22,7 @@ echo "$PPID/$1 up after $start_time seconds"
 failures=0
 
 while true; do
+	kill -0 $PPID &> /dev/null || (echo "$PPID/$1 died unexpectedly"; exit)
 	if ! curl -fsm 30 http://127.0.0.1:"$1"/recent.rss > /dev/null; then
 		failures=$(( $failures + 1 ))
 		echo "$PPID/$1 timed out ($failures/3)"
