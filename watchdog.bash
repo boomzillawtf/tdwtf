9e16d20dbae65d0c2c893601ec08e152e8817dfb
Ben Lubar <ben.lubar@gmail.com>
2016-11-17 02:02:18 -0600

    use current CPU percentage, not overall CPU percentage

--- watchdog.bash
+++ watchdog.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 history_count=20
-threshold="`printf "%05.1f" "90.0" | sed -e 's/\.//'`"
+threshold=$(( 90 * ( $history_count - 1 ) * 5 / $(getconf CLK_TICK) ))
 
 for (( i=0; $i <= $history_count; i++ )); do
 	eval declare -A history$i="([1]=0)"
@@ -9,34 +9,29 @@ done
 
 while true; do
 	unset history$history_count
-	eval "declare -A history$history_count=([1]=0 $(ps -C 'node' -o 'pid=,pcpu=' | awk '{ if ($1 != "1") print $1 "," $2; }' | while read line; do
-		pid="`cut -d, -f1 <<< "$line"`"
-		pcpu="`cut -d, -f2 <<< "$line"`"
-		pcpu="`printf "%05.1f" "$pcpu" | sed -e 's/\.//'`"
+	eval "declare -A history$history_count=([1]=0 $(for pid in $(pidof node); do
+		if [[ "$pid" == "1" ]]; then
+			continue
+		fi
 
-		echo "[$pid]=$pcpu"
+		declare -a stat=($(cat /proc/$pid/stat))
+		echo "[$pid]=${stat[13]}"
+		unset stat
 	done))"
 
 	for pid in "${!history0[@]}"; do
-		fail=""
-		for (( i=0; $i <= $history_count; i++ )); do
-			pcpu="history$i[$pid]"
-			pcpu="${!pcpu}"
-			if [[ -z "$pcpu" ]]; then
-				fail=yes
-				break
-			fi
-
-			if (( 10#$pcpu < 10#$threshold )); then
-				fail=yes
-				break
-			fi
-		done
-
-		if [[ -z "$fail" ]]; then
-			echo '{"level":"error","message":"[watchdog.bash] killing '"$pid"'","timestamp":"'"`date -u --iso-8601=ns | sed -e 's/......+00:00/Z/g' -e 's/,/./g'`"'"}'
-			kill -9 "$pid"
+		pcpu="history$history_count[$pid]"
+		pcpu="${!pcpu}"
+		if [[ -z "$pcpu" ]]; then
+			continue
 		fi
+
+		if (( $pcpu - "${history0[$pid]}" < $threshold )); then
+			continue
+		fi
+
+		echo '{"level":"error","message":"[watchdog.bash] killing '"$pid"'","timestamp":"'"`date -u --iso-8601=ns | sed -e 's/......+00:00/Z/g' -e 's/,/./g'`"'"}'
+		kill -9 "$pid"
 	done
 
 	for (( i=0; $i < $history_count; i++ )); do
