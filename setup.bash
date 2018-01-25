4d6fca4607072306e658b4a982abc8e7bfabdaff
Ben Lubar <ben.lubar@gmail.com>
2018-01-25 15:48:26 -0600

    Remove Redis from the setup.

--- setup.bash
+++ setup.bash
@@ -43,16 +43,15 @@ docker pull boomzillawtf/tdwtf
 # create a Docker network
 docker network create --subnet 172.21.1.0/24 wtdwtf
 
-# create the redis container
-docker run -d --name wtdwtf-redis --net wtdwtf --restart unless-stopped redis redis-server /data/redis.conf
+# create the MongoDB container.
+docker run -d --name wtdwtf-mongo --net wtdwtf --restart unless-stopped mongo --replSet rs
 
-# stop the container, copy the config into its volume, and restart it
-docker stop wtdwtf-redis
-docker cp redis.conf wtdwtf-redis:/data/redis.conf
-docker start wtdwtf-redis
+# wait for MongoDB to be online and create the replSet.
+while ! docker exec wtdwtf-mongo mongo --eval 'db.version()' > /dev/null 2>&1; do
+	sleep 1
+done
 
-# create the MongoDB container. we use mmapv1 on WTDWTF.
-docker run -d --name wtdwtf-mongo --net wtdwtf --restart unless-stopped mongo
+docker exec wtdwtf-mongo mongo --eval 'rs.initiate()'
 
 # give MongoDB time to start up
 until nc -z "`docker inspect -f '{{ .NetworkSettings.Networks.wtdwtf.IPAddress }}' wtdwtf-mongo`" 27017; do echo "Waiting for MongoDB"; sleep 1; done
@@ -61,7 +60,7 @@ until nc -z "`docker inspect -f '{{ .NetworkSettings.Networks.wtdwtf.IPAddress }
 docker run -d --name wtdwtf-nodebb --net wtdwtf --ip 172.21.1.254 --restart unless-stopped -v /usr/share/nginx/wtdwtf-nodebb.config:/usr/src/app/docker -v /usr/share/nginx/wtdwtf-nodebb.uploads:/usr/src/app/public/uploads boomzillawtf/tdwtf
 
 # run the setup script
-docker exec wtdwtf-nodebb node app --setup='{"url":"http://nodebb.local","secret":"not-secret wtdwtf test","database":"mongo","mongo:host":"wtdwtf-mongo","mongo:port":27017,"mongo:username":"","mongo:password":"","mongo:database":"0","redis:host":"wtdwtf-redis","redis:port":6379,"redis:password":"","redis:database":0,"admin:username":"PaulaBean","admin:email":"paula@example.com","admin:password":"brillant","admin:password:confirm":"brillant"}'
+docker exec wtdwtf-nodebb node app --setup='{"url":"http://nodebb.local","secret":"not-secret wtdwtf test","database":"mongo","mongo:host":"wtdwtf-mongo","mongo:port":27017,"mongo:username":"","mongo:password":"","mongo:database":"0","admin:username":"PaulaBean","admin:email":"paula@example.com","admin:password":"brillant","admin:password:confirm":"brillant"}'
 
 # copy the complete config
 docker cp config.json wtdwtf-nodebb:/usr/src/app/docker/config.json
