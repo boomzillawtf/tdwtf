ad078f489fa0218dc1ca2848902db79cacdf122f
Ben Lubar <ben.lubar@gmail.com>
2018-04-01 14:59:16 -0500

    Allow overriding CSS triggers using URL parameters

--- plugins/nodebb-plugin-tdwtf-customizations/style.js
+++ plugins/nodebb-plugin-tdwtf-customizations/style.js
@@ -1,9 +1,20 @@
 /* jshint browser: true */
 /* globals $, ajaxify, app */
+var initialSearch = location.search || '?';
+
 $(window).on('action:ajaxify.contentLoaded', function() {
 	var $html = $('html');
 
 	function data(name, value) {
+		var params = initialSearch.substring(1).split(/&/g);
+		for (var i = 0; i < params.length; i++) {
+			var param = params[i].split(/=/);
+			if (decodeURIComponent(param[0]) === 'override_' + name) {
+				$html.attr('data-' + name, decodeURIComponent(param[1]));
+				return;
+			}
+		}
+
 		if (value) {
 			$html.attr('data-' + name, value === true ? '' : value);
 		} else {
@@ -27,10 +38,10 @@ $(window).on('action:ajaxify.contentLoaded', function() {
 	data('impossible-mission-b', app.user && app.user.isImpossibleMissionB);
 
 	var now = new Date();
-	$html.attr('data-current-year', now.getFullYear());
-	$html.attr('data-current-month', now.getMonth() + 1);
-	$html.attr('data-current-day-of-week', now.getDay());
-	$html.attr('data-current-day-of-month', now.getDate());
+	data('current-year', now.getFullYear());
+	data('current-month', now.getMonth() + 1);
+	data('current-day-of-week', now.getDay());
+	data('current-day-of-month', now.getDate());
 
 	if ($('#new_topic').length && $('#new-topics-alert').length) {
 		$('#new-topics-alert').css('margin-left', $('#new_topic').outerWidth() + 10 + 'px');
